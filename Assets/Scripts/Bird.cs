419edc82f720079a83ccc03bc7a0e64191aecd4f
ugurgolpinar <golpinarugur@gmail.com>
2020-02-03 01:13:01 +0300

    first commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private Rigidbody2D rb;
+    private Vector3 startPosition;
+    private float timeSittingAround;
+    private bool birdWasLaunched;
+
+    public float forceRate;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, startPosition);
+
+        if (birdWasLaunched && rb.velocity.magnitude < 0.1f)
+        {
+            timeSittingAround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 7f || transform.position.y < -10f || timeSittingAround > 3f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        Vector2 directionToStartPosition = startPosition - transform.position;
+        rb.AddForce(directionToStartPosition * forceRate);
+
+        rb.gravityScale = 1f;
+        birdWasLaunched = true;
+        lineRenderer.enabled = false;
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y, 0);
+
+        lineRenderer.enabled = true;
+    }
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject cloudParticleEffect;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Bird bird = other.collider.GetComponent<Bird>();
+        if (bird != null)
+        {
+            Instantiate(cloudParticleEffect, new Vector3(transform.position.x, transform.position.y, -2f), Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        if (other.contacts[0].normal.y < -0.5f)
+        {
+            Instantiate(cloudParticleEffect, new Vector3(transform.position.x, transform.position.y, -2f), Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+    }
+}
